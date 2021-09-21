0431a6b57e6b552e5d71af546fa900fbb3c1a59d
woodaaron2001 <woodaaron2001@gmail.com>
2021-09-21 14:22:05 +0100

    Added camera angles gun object and sound effects

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+import time
+
+start_time = time.time()
+
 
 class LandObject(Entity):
     def __init__(self,position = (0,0,0),scale = (100,1,100)):
@@ -12,7 +16,7 @@ class LandObject(Entity):
             color = color.white,
             collider = 'box'
             )
-    
+
 class targetObject(Button):
     def __init__(self,position = (0,0,0),scale = 2):
         super().__init__(
@@ -25,8 +29,19 @@ class targetObject(Button):
             highlight_color = color.lime
             )
 
+class gunCamera(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = camera.ui,
+            model = 'gun.obj',
+            scale = 0.01,
+            rotation = Vec3(80,10,0),
+            position= Vec2((0.4,-0.4)),
+            texture = 'M9_BaseColor.png'
+            )
+            
 class gameButton(Button):
-    def __init__(self,position = (0,0,0),text = "default"):
+    def __init__(self,position = (0,0,0),text = "default",stateGiven = 1):
         super().__init__(
             parent = scene,
             icon = 'sword',
@@ -38,8 +53,10 @@ class gameButton(Button):
             )
         def input(self,key):
             if self.hovered:
+                text = Text(text = "fuck" ,scale = 10, position = (1,1,1))
                 if key == 'left mouse down':
-                    text = Text(text = "fuck" ,scale = 10, position = (1,1,1))
+                    state = stateGiven
+                   
         
 class EnemyObject(Button):
     def __init__(self,position = (0,0,0),scale = (1,5,1)):
@@ -59,14 +76,31 @@ class EnemyObject(Button):
                 destroy(self)
 
 
+state = 0
 
 def update():
+    global state
+    if (state == 0):
+        camera.x +=1 * time.dt
+        
+
+    if held_keys['#']:
+        player = FirstPersonController(speed = 10, position = (0,35,0),fov= 150)
+        state += 1
+        gun = gunCamera()
+    
     if held_keys['r']:
         camera.fov += 1
+        
     if held_keys['e']:
         camera.fov -= 1
         
+    if(state > 0):
+        if held_keys['left mouse down']:
+            
+        
 app = Ursina()
+
 
 window.title = 'Aiming game'
 window.borderless = False
@@ -74,25 +108,34 @@ window.fullscreen = True
 window.exit_button.visible = False
 window.fps_counter.enabled = True
 
+
+
+camera.fov = 120
+camera.x = -20
+camera.y = 30
+camera.z = -40
+
+
 side1 = LandObject(position = (0,25,50),scale= (100,50,1))
 side2 = LandObject(position = (0,25,-50),scale= (100,50,1))
 side3 = LandObject(position = (50,25,0),scale= (1,50,100))
 side4 = LandObject(position = (-50,25,0),scale= (1,50,100))    
 floor = LandObject(position = (0,0,0) )
-ceiling = LandObject(position = (0,100,0) )
+ceiling = LandObject(position = (0,50,0) )
 
-container1 = LandObject(position = (0,1,2),scale= (3.25,1.5,0.5))
-container1 = LandObject(position = (0,1,-2),scale= (3.25,1.5,0.5))
-container1 = LandObject(position = (2,1,0),scale= (0.5,1.5,3.25))
-container1 = LandObject(position = (-2,1,0),scale= (0.5,1.5,3.25))
+container1 = LandObject(position = (0,1,2),scale= (3.25,30,0.5))
+container2 = LandObject(position = (0,1,-2),scale= (3.25,30,0.5))
+container3 = LandObject(position = (2,1,0),scale= (0.5,30,3.25))
+container4 = LandObject(position = (-2,1,0),scale= (0.5,30,3.25))
+containerFloor = LandObject(position = (0,15,0),scale= (5,0.5,5))
 
-pole1 = LandObject(position = (2,1,2),scale= (0.5,7.5,0.5))
-pole2 = LandObject(position = (-2,1,-2),scale= (0.5,7.5,0.5))
-pole3 = LandObject(position = (-2,1,2),scale= (0.5,7.5,0.5))
-pole4 = LandObject(position = (2,1,-2),scale= (0.5,7.5,0.5))
+pole1 = LandObject(position = (2,1,2),scale= (0.5,43,0.5))
+pole2 = LandObject(position = (-2,1,-2),scale= (0.5,43,0.5))
+pole3 = LandObject(position = (-2,1,2),scale= (0.5,43,0.5))
+pole4 = LandObject(position = (2,1,-2),scale= (0.5,43,0.5))
 
 
-top = LandObject(position = (0,5,0),scale= (5,.5,5))
+top = LandObject(position = (0,22,0),scale= (5,0.5,5))
 
 for x in range(20):
     target = targetObject(position =(random.uniform(-40,40),random.uniform(10,45),48))
@@ -100,11 +143,10 @@ for x in range(20):
 for x in range(20):
     enemy = EnemyObject(position =(random.uniform(-40,40),2.5,random.uniform(0,50)))
 
-button1 = gameButton(position  = (1,1.25,1.7),text = "Horde")
-button2 = gameButton(position  = (-1,1.25,1.7),text = "Sniper")
-player = FirstPersonController()
+button1 = gameButton(position  = (1,16,1.7),text = "Horde")
+button2 = gameButton(position  = (-1,16,1.7),text = "Sniper")
 
 
-camera.fov = 120
+
 
 app.run()
