88aed35940864dee651d891bc72fb850869c153c
woodaaron2001 <woodaaron2001@gmail.com>
2021-09-21 11:57:26 +0100

    Added button entities for gamemode types

--- game.py
+++ game.py
@@ -14,40 +14,57 @@ class LandObject(Entity):
             )
     
 class targetObject(Button):
-    def __init__(self,position = (0,0,0)):
+    def __init__(self,position = (0,0,0),scale = 2):
         super().__init__(
             parent = scene,
             position= position,
-            scale = 2,
+            scale = scale,
             model = 'circle',
-            color = color.gray,
+            color = color.red,
             collider = 'circle',
             highlight_color = color.lime
             )
 
-class EnemyObject(Entity):
-    def __init__(self,position = (0,0,0),scale = (100,1,100)):
+class gameButton(Button):
+    def __init__(self,position = (0,0,0),text = "default"):
+        super().__init__(
+            parent = scene,
+            icon = 'sword',
+            position= position,
+            scale = 0.5,
+            color = color.gray,
+            collider = 'circle',
+            highlight_color = color.lime
+            )
+        def input(self,key):
+            if self.hovered:
+                if key == 'left mouse down':
+                    text = Text(text = "fuck" ,scale = 10, position = (1,1,1))
+        
+class EnemyObject(Button):
+    def __init__(self,position = (0,0,0),scale = (1,5,1)):
         super().__init__(
             parent = scene,
             position= position,
             scale = scale,
             model = 'cube',
             texture = 'white_cube',
-            color = color.white,
-            collider = 'box'
+            color = color.red,
+            collider = 'box',
+            highlight_color = color.blue
             )
-    
+    def input(self,key):
+        if self.hovered:
+            if key == 'left mouse down':
+                destroy(self)
+
+
 
 def update():
     if held_keys['r']:
         camera.fov += 1
     if held_keys['e']:
         camera.fov -= 1
-    if held_keys['f']:
-        Rope = RopeObject()
-
-        #create a swing/robe object which originates at players origin and 
-
         
 app = Ursina()
 
@@ -57,35 +74,37 @@ window.fullscreen = True
 window.exit_button.visible = False
 window.fps_counter.enabled = True
 
-land1 = LandObject(position = (0,0,0))
-
-land2 = LandObject(position = (0,50,0))
-
 side1 = LandObject(position = (0,25,50),scale= (100,50,1))
-
 side2 = LandObject(position = (0,25,-50),scale= (100,50,1))
-
 side3 = LandObject(position = (50,25,0),scale= (1,50,100))
-
 side4 = LandObject(position = (-50,25,0),scale= (1,50,100))    
+floor = LandObject(position = (0,0,0) )
+ceiling = LandObject(position = (0,100,0) )
+
+container1 = LandObject(position = (0,1,2),scale= (3.25,1.5,0.5))
+container1 = LandObject(position = (0,1,-2),scale= (3.25,1.5,0.5))
+container1 = LandObject(position = (2,1,0),scale= (0.5,1.5,3.25))
+container1 = LandObject(position = (-2,1,0),scale= (0.5,1.5,3.25))
+
+pole1 = LandObject(position = (2,1,2),scale= (0.5,7.5,0.5))
+pole2 = LandObject(position = (-2,1,-2),scale= (0.5,7.5,0.5))
+pole3 = LandObject(position = (-2,1,2),scale= (0.5,7.5,0.5))
+pole4 = LandObject(position = (2,1,-2),scale= (0.5,7.5,0.5))
 
 
+top = LandObject(position = (0,5,0),scale= (5,.5,5))
 
-container1 = LandObject(position = (0,1,2),scale= (3.25,2,0.5))
-container1 = LandObject(position = (0,1,-2),scale= (3.25,2,0.5))
-container1 = LandObject(position = (2,1,0),scale= (0.5,2,3.25))
-container1 = LandObject(position = (-2,1,0),scale= (0.5,2,3.25))
-
-pole1 = LandObject(position = (2,1,2),scale= (0.5,5,0.5))
-pole2 = LandObject(position = (-2,1,-2),scale= (0.5,5,0.5))
-pole3 = LandObject(position = (-2,1,2),scale= (0.5,5,0.5))
-pole4 = LandObject(position = (2,1,-2),scale= (0.5,5,0.5))
-
-top = LandObject(position = (0,5,0),scale= (1,1,1))
 for x in range(20):
     target = targetObject(position =(random.uniform(-40,40),random.uniform(10,45),48))
-    
+
+for x in range(20):
+    enemy = EnemyObject(position =(random.uniform(-40,40),2.5,random.uniform(0,50)))
+
+button1 = gameButton(position  = (1,1.25,1.7),text = "Horde")
+button2 = gameButton(position  = (-1,1.25,1.7),text = "Sniper")
 player = FirstPersonController()
+
+
 camera.fov = 120
 
 app.run()
